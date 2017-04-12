622494b423e22198bfda42e65e2a91bf7c783a81
Bosko8701 <boskito8701github>
2017-04-12 16:26:37 -0500

    Error fatal

--- app/src/main/java/machine/pixel/bucketdrops/ActivityMain.java
+++ app/src/main/java/machine/pixel/bucketdrops/ActivityMain.java
@@ -2,12 +2,17 @@ package machine.pixel.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
     }
 }
