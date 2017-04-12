d2477e7bd0a517178ec4d07d5cdf2e66fec20798
Bosko8701 <boskito8701github>
2017-04-12 17:30:19 -0500

    OnClick event handling

--- app/src/main/java/machine/pixel/bucketdrops/ActivityMain.java
+++ app/src/main/java/machine/pixel/bucketdrops/ActivityMain.java
@@ -3,16 +3,44 @@ package machine.pixel.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolBar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener () {
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(ActivityMain.this, "El boton ha sido puchado", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.boton_agregar);
+
+        mBtnAdd.setOnClickListener(mBtnAddListener);
         setSupportActionBar(mToolBar);
+        initBackgroundImage();
     }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.fondo)
+                .centerCrop()
+                .into(background);
+    }
+
 }
